1af78bb23462a9514456d779e5c97caca8a0c301
Adriane35448 <158136274+Adriane35448@users.noreply.github.com>
2025-10-03 20:16:09 +0800

    Add files via upload

---
+++ MyStudDetails.java
@@ -0,0 +1,8 @@
+
+public class MyStudDetails {
+
+	public static void main(String[] args) {
+		System.out.print("My Name is Adriane Cuanico, I am a freshmen student from college of science representing IT department!");
+	}
+
+}
