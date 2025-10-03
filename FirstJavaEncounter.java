c9421644dc8fb30b4d02e7a345b60398ae2fb999
Adriane35448 <158136274+Adriane35448@users.noreply.github.com>
2025-10-03 20:17:17 +0800

    Add files via upload

---
+++ FirstJavaEncounter.java
@@ -0,0 +1,15 @@
+ 
+public class FirstJavaEncounter {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Computer Programming 1 - CC102\n");
+		System.out.println("Introduction to Computing - CC101\n");
+		System.out.println("Retorika - Filipino 1\n");
+		System.out.println("Environmental Science - GE ELEC 1\n");
+		System.out.println("Computer Bookkeeping and Accounting - IT101\n");
+		System.out.println("IT Professional Ethics - ITP102\n");
+		System.out.println("Web System and Technologies - WS101\n");
+	}
+
+}
