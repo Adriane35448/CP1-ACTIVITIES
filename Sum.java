73c32d1211a9514429f681090f8ad89ac54050ce
Adriane35448 <158136274+Adriane35448@users.noreply.github.com>
2025-10-03 20:12:26 +0800

    fourth-commit

---
+++ Sum.java
@@ -0,0 +1,18 @@
+
+public class Sum {
+	
+    public static void main(String[] args) {
+    	
+        int sum = 0;        
+        for (int x = 96; x >= 74; x--) {
+        	
+            System.out.println(x);
+            sum += x;
+        }
+
+        
+        System.out.println("\nThe sum is : " + sum);
+    }
+
+
+}
