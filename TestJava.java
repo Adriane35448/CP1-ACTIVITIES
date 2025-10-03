1252f326f6310d77e48af2bc612c5e080e0caf37
Adriane35448 <158136274+Adriane35448@users.noreply.github.com>
2025-10-03 20:10:57 +0800

    third-commit

---
+++ TestJava.java
@@ -0,0 +1,19 @@
+
+public class TestJava {
+
+	public static void main(String[] args) {
+
+		int y = 50;
+		
+	
+		if(y % 2 == 0) {
+			System.out.println("True");
+		}
+		
+		else {
+			System.out.println("False");	
+		}
+
+	}
+
+}
