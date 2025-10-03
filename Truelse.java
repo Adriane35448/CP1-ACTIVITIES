8ea78a9aa184b0ca964909ba665d33bdf05dcade
Adriane35448 <158136274+Adriane35448@users.noreply.github.com>
2025-10-03 20:18:22 +0800

    Add files via upload

---
+++ Truelse.java
@@ -0,0 +1,18 @@
+
+public class Truelse {
+
+	public static void main(String[] args) {
+
+		int y = 30;
+		
+		
+		if(y % 2 == 0) {
+
+	System.out.println("true");
+		}
+		else
+    System.out.println("false");
+
+	}
+
+}
